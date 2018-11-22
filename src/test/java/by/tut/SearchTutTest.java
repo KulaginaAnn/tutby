309c3c8d797a88e719fbc2167ebc16309c45a006
Anna Kulagina <anna.kulahina@gmail.com>
2018-11-22 11:00:18 +0300

    import Assert, add Logger, delete @Test, rename variables and method

--- src/test/java/by/tut/SearchTutTest.java
+++ src/test/java/by/tut/SearchTutTest.java
@@ -2,18 +2,16 @@ package by.tut;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 /**
  * Created by Ann on 14.11.18
@@ -23,88 +21,99 @@ import java.util.concurrent.TimeUnit;
 /**test tut.by search job*/
 public class SearchTutTest {
 
-    private static final String TEST_QUERY = "QA automation engineer";//input data
+    private static  Logger log ;
     private static WebDriver driver;
+
+    private static final String TAB = "Работа";
+    private static final String TEST_QUERY = "QA Automation Engineer";//input data
+
+    private static String inputVacancyClassName = "bloko-control-group__main";
+    private static String buttonSearchVacancyCssSelector= "button.bloko-button.bloko-button_primary.bloko-button_stretched";
+    private static String divFoundVacanccyClassName = "search-item-name";
+
 
 
     /**initialization driver */
-    @BeforeClass
+    @BeforeTest
     public static void setUp() {
         File file = new File("C:/selenium/chromedriver_win32/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         driver = new ChromeDriver();//initialization driver
         driver.manage().window().maximize();
         driver.get("https://www.tut.by/");
+        log = Logger.getLogger("new logger");
+    }
+
+
+    @Test
+    public void searchJobOnTutby() {
+
+        log.info("start searchJob");
+        searchTabJob();
+        log.info("end searchJob");
+        log.info("start input data");
+        inputSearchVacancyName();
+        log.info("end input data");
+        log.info("start click button");
+        clickButtonToSearchVacancy();
+        log.info("end click button");
+        log.info("start check");
+        matchCheckVacancy();
+        log.info("end check");
     }
 
     /**
      * search and click job
      */
-    @Test
-    public void searchJob() {
-        WebElement searchJob = driver.findElement(By.linkText("Работа"));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        searchJob.click();
+    public void searchTabJob() {
+        log.info("Step 1: Move to “Работа” category");
+        WebElement tabJob = driver.findElement(By.linkText(TAB));
+        tabJob.click();
     }
 
     /**
      * input data into input
      */
-    @Test(priority = 1)
-    public void inputData() {
-        WebElement input = driver.findElement(By.cssSelector(" div.bloko-control-group__main"));
-
+    public void inputSearchVacancyName() {
+        log.info("Step 2: Entering text data into input for job search ");
+        WebElement inputVacancy = driver.findElement(By.className (inputVacancyClassName));
         Actions actions = new Actions(driver);
-        actions.moveToElement(input);
+        actions.moveToElement(inputVacancy);
         actions.click();
         actions.sendKeys(TEST_QUERY);
         actions.build().perform();
     }
 
     /**click button*/
-    @Test(priority = 2)
-    public void clickButton() {
-        WebElement button = driver.findElement(By.cssSelector(" button.bloko-button.bloko-button_primary.bloko-button_stretched"));
 
-        Actions action = new Actions(driver);
-        action.moveToElement(button);
-        action.click();
-        action.build().perform();
+    public void clickButtonToSearchVacancy() {
+        log.info("Step 3: Click button job search ");
+
+        WebElement buttonSearchVacancy = driver.findElement(By.cssSelector(buttonSearchVacancyCssSelector));
+        buttonSearchVacancy.click();
     }
 
     /**check matches whith input data*/
-    @Test(priority = 3)
-    public void matchCheck() {
-        int numberOfMatches = 0;
-        int numberOfNotMatches = 0;
 
-        List<WebElement> listDiv = driver.findElements(By.cssSelector("div.search-item-name "));
-
-        if (listDiv.size() > 0) {
-            for (WebElement list : listDiv) {
-
-                try {
-                    list.findElement(By.partialLinkText("QA"));
-                    numberOfMatches++;
-
-                } catch (NoSuchElementException e) {
-                    numberOfNotMatches++;
-                }
-
+    public void matchCheckVacancy() {
+        log.info("Step 4: match check ");
+        List<WebElement> listDivFoundVacancy = driver.findElements(By.className(divFoundVacanccyClassName));
+        System.out.println("Всего записей: " + listDivFoundVacancy.size());
+        Assert.assertNotNull(listDivFoundVacancy);
+            for (WebElement list : listDivFoundVacancy) {
+                Assert.assertEquals(list.getText(), TEST_QUERY);
+                Assert.fail("there is a mismatch");
             }
-        }
-        System.out.println("Всего записей: " + listDiv.size());
-        System.out.println("количество совпадений: " + numberOfMatches);
-        System.out.println("не удовлетворяет поиску: " + numberOfNotMatches);
     }
 
 
         /**close*/
         @AfterClass
-        public static void closed() {
+        public static void teardown() {
             driver.quit();
 
         }
+
 
 
 }
